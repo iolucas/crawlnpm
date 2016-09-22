398b52e7bebb9c0b0dbfdadcc7f4794aa1ec3612
Lucas <lucas.o@live.com>
2016-09-21 21:05:51 -0300

    created

---
+++ app.js
@@ -0,0 +1,371 @@
+var async = require("async");
+var neo4j = require('node-neo4j');
+var npmApi = require("./npmapi.js");
+var fs = require("fs");
+
+//Utils
+var print = console.log;
+
+//Create error log file
+var errorLog = "------------ NPM Crawl Error Log ------------\r\n\r\n\r\n";
+var errorLogFileName = "logs/error_log_" + Math.random()*1000 + ".txt";
+fs.writeFileSync(errorLogFileName, errorLog);
+
+var writeErrorLog = function(log) {
+    if(log == null)
+        return;
+
+    var logText;
+    
+    try {
+        logText = JSON.stringify(log);
+    } catch (e){
+        logText = log;
+    } finally {
+        //errorLog += logText + "\r\n\r\n\r\n";
+        fs.appendFileSync(errorLogFileName, logText + "\r\n\r\n\r\n");
+    }
+    
+}
+
+var db = new neo4j('http://neo4j:lucas@localhost:7474');
+
+//Define constraints
+var modulePathConstraint = "CREATE CONSTRAINT ON (module:Module) ASSERT module.path IS UNIQUE";
+
+print("Initing db...")
+//Execute constraint set query
+db.cypherQuery(modulePathConstraint, function(err) {
+    if(err)
+        print(err);
+    else {
+        print("Db initiated");
+        main(process.argv);
+    }
+});
+
+
+function main(argv) {
+
+    switch(argv[2]) {
+
+        case "--all-empty":
+            var limit = argv[3] || 10;
+            //Get all modules that does not have a title
+            db.cypherQuery("MATCH (n:Module) WHERE n.title", function(err, result) {
+                callback(err, result);
+            });    
+
+
+    }
+
+
+    crawlNpmPathCollection([argv[2]], function() {
+        console.log("DONE");
+    });
+
+
+
+    /*crawlCollection(['cfenv', 'express'], function() {
+        console.log("DONE");
+    });*/
+
+
+    /*crawlUnique(argv[2], function() {
+        console.log(arguments);
+
+    });*/
+
+
+    /*npmApi.getNpmModuleData(process.argv[2], function(err, data) {
+        console.log(err);
+        console.log(data);
+    });*/
+
+}
+
+
+
+//Crawl any collection of wikipedia urls
+function crawlNpmPathCollection(urlCollection, callback) {
+
+    var urlQty = urlCollection.length;
+    var downloadsLeft = urlCollection.length;
+    var doneQty = 0;
+
+    print("Pages left: " + downloadsLeft);
+
+    var urlCollectionEmptyFlag = false;
+
+    //Queue to handle addition of items into the database
+    var databaseQueue = async.queue(function(moduleData, taskCallback) {
+        
+        //Add this module data to the database
+        addNpmModuleToDb(moduleData, function(err) {
+            if(err)
+                console.log(err);
+
+            taskCallback(err, moduleData.path);
+        });
+
+    }, 1);
+
+    //Callback to be called when the database queue are empty
+    databaseQueue.drain = function() {
+        console.log("Database queue is empty.");
+
+        //If the url collection to be download is empty, call the finish callback
+        if(urlCollectionEmptyFlag)
+            callback();
+    }
+
+    //Queue to handle the download of the wikipedia pages
+    var npmPathQueue = async.queue(function(path, taskCallback) {
+
+        print("Getting data of module: '" + path + "'");
+
+        npmApi.getNpmModuleData(path, function(error, moduleData) {
+            downloadsLeft--;
+            
+            //If error, exit with it
+            if(error) {
+                taskCallback(getErrorString(error, 
+                    "Crawl Error with module: '" + path + "': "));
+                return;
+            }
+
+            //Push this pageinfo to the database queue
+            databaseQueue.push(moduleData, function(err, modulePath) {
+                doneQty++;
+                if(err) {
+                    var errorString = getErrorString(err, 
+                        "Error while adding data to database from url " + modulePath + ": ");
+                    print(errorString);
+                    writeErrorLog(errorString);
+                } else {
+                    print("Module '" + modulePath + "' added to the database.");
+                } 
+                print("Modules done: " + doneQty + "/" + urlQty);
+            
+                //Call the task finish callback
+                taskCallback(null, path);
+
+            });
+
+
+        });
+
+    }, 1);
+
+    //Callback to be called when the wikipages queue are empty
+    npmPathQueue.drain = function() {
+        print("Npm paths queue is empty.");
+        urlCollectionEmptyFlag = true;
+    }
+
+    npmPathQueue.push(urlCollection, function(err, path){
+        if(err) {
+            var errorString = getErrorString(err, "Error while downloading page: " + path);
+            print(errorString);
+            writeErrorLog(errorString);
+        } else {
+            print("Module '" + path + "' downloaded. Modules left: " + downloadsLeft);
+        }
+    });
+}
+
+
+function addNpmModuleToDb(moduleData, callback) {
+
+    var data = moduleData;
+
+    //Create query to create or update module data
+    var createOrMatchQuery = "MERGE (n:Module {path:'" + data.path +"'})";
+
+    //Query to update stuff
+    var updateOptions = {
+        title: data.title,
+        description: data.description || "",
+        githubAddr: data.githubAddr || "",
+        //lastPublishUser: data.lastPublisher.publisher || "",
+        //lastPublishDate: data.lastPublisher.date || "",
+        //lastPublishDateFormat: data.lastPublisher.format || "",
+        lastRelease: data.lastRelease || "",
+        dailyDownloads: data.stats.dailyDownloads || "",
+        weeklyDownloads: data.stats.weeklyDownloads || "",
+        monthlyDownloads: data.stats.monthlyDownloads || ""
+    }
+
+    var createQuery = "";
+
+    createQuery += "SET";
+    for (var key in updateOptions) {
+        var value = updateOptions[key];
+        if((typeof value) == 'string')
+            value = '"' + value + '"';
+
+        createQuery += " n." + key + " = " + value + ", ";
+    }
+    createQuery = createQuery.substr(0, createQuery.length - 2);
+
+
+    //Query to create keywords
+    var keywordsQuery = "";
+    for(var i = 0; i < data.keywords.length; i++) {
+        var keywordTitle = data.keywords[i];
+        keywordsQuery += "MERGE (k" + i + ":Keyword {title:'" + keywordTitle +"'}) CREATE UNIQUE (k" + i + ")<-[:Keyword]-(n) ";
+    }
+
+    //Query to create dependences
+    var dependentsQuery = "";
+    //for (var key in data.dependenciesPaths) {
+    for(var i = 0; i < data.dependenciesPaths.length; i++) {
+        var dependencePath = data.dependenciesPaths[i];
+        dependentsQuery += "MERGE (dc" + i + ":Module {path:'" + dependencePath +"'}) CREATE UNIQUE (dc" + i + ")<-[:DependsOf]-(n) ";
+    }
+
+    //Query to create dependents
+    for(var i = 0; i < data.dependentsPaths.length; i++) {
+        var dependentPath = data.dependentsPaths[i];
+        dependentsQuery += "MERGE (dt" + i + ":Module {path:'" + dependentPath +"'}) CREATE UNIQUE (dt" + i + ")-[:DependsOf]->(n) ";
+    }
+
+    //console.log(keywordsQuery);
+
+    var neoQuery = [createOrMatchQuery, createQuery, keywordsQuery,dependentsQuery].join(" ");
+    //console.log(neoQuery);
+
+    db.cypherQuery(neoQuery, function(err, result) {
+        callback(err, result);
+    });
+}
+
+
+
+function crawlCollection(paths, callback) {
+
+    var asyncQueue = async.queue(function(path, taskCallback) {
+        print("Crawling " + path + " ...");
+        crawlUnique(path, function(err) {
+            if(err)
+                console.log(err);
+
+            print("Done with " + path + ".");
+            taskCallback();
+        });
+
+    },1);
+
+    asyncQueue.drain = function() {
+        callback();
+    }
+
+    asyncQueue.push(paths, function() {
+        console.log(arguments);
+    });
+}
+
+function crawlUnique(path, callback) {
+
+    npmApi.getNpmModuleData(path, function(err, data) {
+        if(err)
+            return callback(err);
+
+        console.log(data);
+
+        //Create query to create or update module data
+        var createOrMatchQuery = "MERGE (n:Module {path:'" + data.path +"'})";
+
+        //Query to update stuff
+        var updateOptions = {
+            title: data.title,
+            description: data.description || "",
+            githubAddr: data.githubAddr || "",
+            //lastPublishUser: data.lastPublisher.publisher || "",
+            //lastPublishDate: data.lastPublisher.date || "",
+            //lastPublishDateFormat: data.lastPublisher.format || "",
+            lastRelease: data.lastRelease || "",
+            dailyDownloads: data.stats.dailyDownloads || "",
+            weeklyDownloads: data.stats.weeklyDownloads || "",
+            monthlyDownloads: data.stats.monthlyDownloads || ""
+        }
+
+        var createQuery = "";
+
+        createQuery += "SET";
+        for (var key in updateOptions) {
+            var value = updateOptions[key];
+            if((typeof value) == 'string')
+                value = '"' + value + '"';
+
+            createQuery += " n." + key + " = " + value + ", ";
+        }
+        createQuery = createQuery.substr(0, createQuery.length - 2);
+
+
+        //Query to create keywords
+        var keywordsQuery = "";
+        for(var i = 0; i < data.keywords.length; i++) {
+            var keywordTitle = data.keywords[i];
+            keywordsQuery += "MERGE (k" + i + ":Keyword {title:'" + keywordTitle +"'}) CREATE UNIQUE (k" + i + ")<-[:Keyword]-(n) ";
+        }
+
+        //Query to create dependences
+        var dependentsQuery = "";
+        //for (var key in data.dependenciesPaths) {
+        for(var i = 0; i < data.dependenciesPaths.length; i++) {
+            var dependencePath = data.dependenciesPaths[i];
+            dependentsQuery += "MERGE (dt" + i + ":Module {path:'" + dependencePath +"'}) CREATE UNIQUE (dt" + i + ")<-[:DependsOf]-(n) ";
+        }
+
+        //Query to create dependents
+        for(var i = 0; i < data.dependentsPaths.length; i++) {
+            var dependentPath = data.dependentsPaths[i];
+            dependentsQuery += "MERGE (dc" + i + ":Module {path:'" + dependentPath +"'}) CREATE UNIQUE (dc" + i + ")-[:DependsOf]->(n) ";
+        }
+
+        //console.log(keywordsQuery);
+
+        var neoQuery = [createOrMatchQuery, createQuery, keywordsQuery,dependentsQuery].join(" ");
+        //console.log(neoQuery);
+
+        db.cypherQuery(neoQuery, function(err, result) {
+            callback(err, result);
+        });
+    });
+}
+
+
+function getErrorString(errorObj, errorMsg) {
+    var errorString;
+    try {
+        errorString = JSON.stringify(errorObj);
+    } catch(e) {
+        errorString = errorObj;
+    } finally {
+        return errorMsg + errorString;
+    }
+}
+
+
+
+/*request("https://www.npmjs.com/package/colors", function(error, response, body) {
+    $ = cheerio.load(body);
+
+    var pageTitle = $('h1.package-name a').html();
+    var description = $('p.package-description').html();
+    var githubAddr;
+
+    $('ul.box li a').each(function(){
+        if(githubAddr != undefined)
+            return;
+
+        var href = $(this).attr('href');
+        if(href.match(/github\.com/))
+            githubAddr = href;
+    });
+
+    console.log(pageTitle);
+    console.log(description);
+    console.log(githubAddr);
+});*/
---
+++ npmapi.js
@@ -0,0 +1,104 @@
+//Module to get data from npm site
+
+//Module to perform http requests
+var request = require("request");
+
+//Module to parse html content "jquery like"
+var cheerio = require("cheerio");
+
+module.exports = {
+    getNpmModuleData: getNpmModuleData    
+}
+
+function getNpmModuleData(modulePath, callback) {
+
+    request("https://www.npmjs.com/package/" + modulePath, function(error, response, body) {
+        if(error)
+            return callback(error, response, body);
+
+        $ = cheerio.load(body);
+
+        var pagePath = modulePath;
+        var pageTitle = $('h1.package-name a').html();
+        var description = $('p.package-description').html();
+
+        var githubAddr;
+        var lastPublisher = {
+            publisher: $("li.last-publisher").children('a').attr('href').substr(2),
+            date: $("li.last-publisher").children('span').attr('data-date'),
+            format: $("li.last-publisher").children('span').attr('data-date-format')
+        }
+
+        var lastRelease;
+        
+        $("div.sidebar ul.box").children("li").not(".last-publisher").children("strong").each(function(){
+            if(lastRelease == undefined)
+                lastRelease = $(this).text();
+        });
+
+        var stats;
+        var statsNode = false;
+        $("div.sidebar").children().each(function(index, tagData) {
+            if(statsNode && !stats ) {
+                stats = {
+                    dailyDownloads: $(this).find("strong.daily-downloads").text(),
+                    weeklyDownloads: $(this).find("strong.weekly-downloads").text(),
+                    monthlyDownloads: $(this).find("strong.monthly-downloads").text(),
+                }
+                
+            } else if(tagData.name == 'h3' && $(this).text() == 'Stats'){
+                statsNode = true;
+            }
+        });
+
+        var keywords = [];
+
+        var dependenciesPaths = [];
+        var dependentsPaths = [];
+
+        //Get github addr
+        $('ul.box li a').each(function(){
+            if(githubAddr != undefined)
+                return;
+
+            var href = $(this).attr('href');
+            if(href.match(/github\.com/))
+                githubAddr = href;
+        });
+
+        //Get keywords
+        $('p.list-of-links a').each(function() {
+            var match = $(this).attr("href").match(new RegExp("^/browse/keyword/(.+)"));
+            if(match)
+                keywords.push(match[1]);
+        });
+
+        //Get dependencies paths
+        $('div.sidebar p.list-of-links:not(".dependents") a').each(function() {
+            var match = $(this).attr("href").match(new RegExp("^/package/(.+)"));
+            if(match)
+                dependenciesPaths.push(match[1]);
+        });
+
+        //Get dependents paths
+        $('div.sidebar p.list-of-links.dependents a').each(function() {
+            var match = $(this).attr("href").match(new RegExp("^/package/(.+)"));
+            if(match)
+                dependentsPaths.push(match[1]);
+        });
+
+        //Fire callback successfully
+        callback(null, {
+            title: pageTitle,
+            description: description,
+            keywords: keywords,
+            path: pagePath,
+            githubAddr: githubAddr,
+            lastPublisher: lastPublisher,
+            lastRelease: lastRelease,
+            stats: stats,
+            dependenciesPaths: dependenciesPaths,
+            dependentsPaths: dependentsPaths
+        });
+    });
+}
